28b15fbc77981c6584b66fcf6e3a5fe9aef5dbfd
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-11-20 17:56:57 +0530

    Baka

--- Skynet_System/strings.py
+++ Skynet_System/strings.py
@@ -11,20 +11,20 @@ on_string = """
 # If you still wanna change it change the regex too
 scan_request_string = """
 ╒═══「 $SCAN 」
-Skynet X Scan request!
-**❂ Enforcer:** {enforcer} 
-**❂ User scanned:** {spammer}
-**❂ Reason:** `{reason}`
-**❂ Scan Source:** {chat}
-**❂ Target Message:** `{message}`
+Lovely X Scan request!
+**✯ Enforcer:** {enforcer} 
+**✯ User scanned:** {spammer}
+**✯ Reason:** `{reason}`
+**✯ Scan Source:** {chat}
+**✯ Target Message:** `{message}`
 """
 forced_scan_string = """
 ╒═══「 $FORCED 」
-**❂ Inspector:** {ins}
-**❂ Target:** {spammer}
-**❂ Reason:** `{reason}`
-**❂ Scan Source:** {chat}
-**❂ Target Message:** `{message}`
+**✯ Inspector:** {ins}
+**✯ Target:** {spammer}
+**✯ Reason:** `{reason}`
+**✯ Scan Source:** {chat}
+**✯ Target Message:** `{message}`
 """
 
 reject_string = """
@@ -43,18 +43,18 @@ proof_string = """
 
 scan_approved_string = """
 ╒═══「 #LethalEliminator 」
-**❂ Target User:** {scam}
-**❂ Crime Coefficient:** `Over 300`
-**❂ Reason:** `{reason}`
-**❂ Enforcer:** `{enforcer}`
-**❂ Case Number:** `{proof_id}`
+**✯ Target User:** {scam}
+**✯ Crime Coefficient:** `Over 300`
+**✯ Reason:** `{reason}`
+**✯ Enforcer:** `{enforcer}`
+**✯ Case Number:** `{proof_id}`
 """
 
 bot_gban_string = """
 ╒═══「 #DestroyDecomposer 」
-**❂ Enforcer:** `{enforcer}`
-**❂ Target User:** {scam}
-**❂ Reason:** `{reason}`
+**✯ Enforcer:** `{enforcer}`
+**✯ Target User:** {scam}
+**✯ Reason:** `{reason}`
 """
 
 # https://psychopass.fandom.com/wiki/Crime_Coefficient_(Index)
