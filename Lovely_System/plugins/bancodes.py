d091d7312f8358e767d996b4b8b7eff7df3ee8ef
Lovely Boy 2 <92840649+lovelygf@users.noreply.github.com>
2021-11-24 14:30:09 +0530

    Baka

---
+++ Lovely_System/plugins/bancodes.py
@@ -0,0 +1,26 @@
+   
+help_plus = """ Here is Help for **Bancodes** :-
+**✯** `{LXSx00}` **SCAMMER.**
+**✯** `{LXSx01}` **SPAM ADDING MEMBER.**
+**✯** `{LXSx03}` **ABUSE SPAM.**
+**✯** `{LXSx04}` **NSFW SPAMMER.**
+**✯** `{LXSx06}` **IMPERSONATION.**
+**✯** `{LXSx07}` **MD/BTC SCAM.**
+**✯** `{LXSx08}` **ADDING SPAMBOTS.**
+**✯** `{LXSx10}` **ILLEGAL.**
+**✯** `{LXSx11}` **PHISHING.**
+**✯** `{LXSx12}` **FRAUD PROMOTION  [ANY KIND].**
+**✯** `{LXSx13}` **CYBER THREATENING / CYBER BULLY.**
+**✯** `{LXSx14}` **CHILD ABUSE.**
+**✯** `{LXSx15}` **BAN EVASION.**
+**✯** `{LXSx16}` **SPAMBOT.**
+**✯** `{LXSx17}` **RAID INITIALIZOR.** 
+**✯** `{LXSx18}` **RAID PARTICIPANT.**
+**✯** `{LXSx19}` **KRIMINALANT.**
+**✯** `{LXSx20}` **SPAMMING VIOLENT CONTENT.**
+**✯** `{LXSx21}` **HATE SPEECH AGAINST NATIONALITY.**
+**✯** `{LXSx22}` **RAID/SPAM INFLAMMER.**
+**✯** `{LXSx23}` **PRONOGRAPHY CCONTENT PROMOTING.**
+**✯** `{LXSx24}` **PAID GIRL.**
+"""
+__plugin_name__ = "bancodes"
