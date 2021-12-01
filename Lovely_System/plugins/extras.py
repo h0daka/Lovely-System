6deed3c17f02960e91473572539fb9437f6d6ee6
Lovely Boy 2 <92840649+lovelygf@users.noreply.github.com>
2021-12-01 12:37:09 +0530

    Update extras.py

--- Lovely_System/plugins/extras.py
+++ Lovely_System/plugins/extras.py
@@ -144,7 +144,7 @@ async def join(event) -> None:
         await System.send_message(event.chat_id, "Joined chat!")
         await System.send_message(
             Lovely_logs,
-            f"{(await event.get_sender()).first_name} made Skynet join {link}",
+            f"{(await event.get_sender()).first_name} made Lovely join {link}",
         )
 
 
