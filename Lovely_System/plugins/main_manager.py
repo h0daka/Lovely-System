d52842798f4529cda7455ff901359d32e636af7a
Lovely Boy 2 <92840649+lovelygf@users.noreply.github.com>
2021-12-01 12:55:12 +0530

    Update main_manager.py

--- Lovely_System/plugins/main_manager.py
+++ Lovely_System/plugins/main_manager.py
@@ -102,7 +102,7 @@ async def scan(event, flags):
         if replied.fwd_from:
             reply = replied.fwd_from
             target = reply.from_id.user_id
-            if reply.from_id.user_id in ENFORCERS or reply.from_id.user_id in Skynet:
+            if reply.from_id.user_id in ENFORCERS or reply.from_id.user_id in Lovely:
                 return
             if not reply.from_id.user_id:
                 await event.reply("Cannot get user ID.")
@@ -123,7 +123,7 @@ async def scan(event, flags):
     if flags.f and executer.id in INSPECTORS:
         approve = True
     else:
-        approve = False
+        approve = True
     if replied.media:
         await replied.forward_to(Lovely_logs)
     executor = f"[{executer.first_name}](tg://user?id={executer.id})"
